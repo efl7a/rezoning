715a7a0421d1ea76a25d128c3499be298379fe54
Heather Christman <efl7a@yahoo.de>
2017-12-12 17:33:08 +0000

    Scraper and Petition Classes are running.  Still need some find methods so that we can give end user information.

--- lib/rezoning/scraper.rb
+++ lib/rezoning/scraper.rb
@@ -13,24 +13,27 @@ class Rezoning::Scraper
     end
   end
   def scrape_petitions
-    self.scrape_year_url.each do |year|
-      doc = Nokogiri::HTML(open("#{BASE_CHARLOTTE_GOV_WEBSITE}#{year}"))
+  #  self.scrape_year_url.each do |year|
+  #    doc = Nokogiri::HTML(open("#{BASE_CHARLOTTE_GOV_WEBSITE}#{year}"))
+  # The city has changed the way they format the petitions from year to year.  For now, this will just allow for the 2017 petitions.
+      doc = Nokogiri::HTML(open("#{BASE_CHARLOTTE_GOV_WEBSITE}#{self.scrape_year_url[0]}"))
       rows = doc.css("tr")
-      binding.pry
       rows[2..-1].each do |petition|
-        current_petition = Rezoning::Petition.new(petition.css("td a").attribute("href").value)
-        current_petition.number = petition.css("td")[0].text
-        current_petition.petitioner = petition.css("td")[1].text
-        current_petition.description = petition.css("td")[2].text
-        current_petition.current_zoning = petition.css("td")[2].text
-        current_petition.proposed_zoning = petition.css("td")[4].text
-        current_petition.decision = petition.css("td")[5].text
-  #      current_petition.decision_date = petition.css("td")[6].text unless petition.css("td")[6].text == nil
-        district_info = petition.css("td")[7].text
-        #"District-Councilperson"
-        current_petition.district = district_info.split("-")[0]
-        current_petition.councilperson = district_info.split("-")[1]
+        if petition.css("td a") != nil
+          current_petition = Rezoning::Petition.new(petition.css("td a").attribute("href").value)
+          current_petition.number = petition.css("td")[0].text unless petition.css("td")[0] == nil
+          current_petition.petitioner = petition.css("td")[1].text unless petition.css("td")[1] == nil
+          current_petition.description = petition.css("td")[2].text unless petition.css("td")[2] == nil
+          current_petition.current_zoning = petition.css("td")[2].text unless petition.css("td")[3] == nil
+          current_petition.proposed_zoning = petition.css("td")[4].text unless petition.css("td")[4] == nil
+          current_petition.decision = petition.css("td")[5].text unless petition.css("td")[5] == nil
+          current_petition.decision_date = petition.css("td")[6].text unless petition.css("td")[6] == nil
+          district_info = petition.css("td")[7].text unless petition.css("td")[7] == nil
+          #"District-Councilperson"
+          current_petition.district = district_info.split("-")[0] unless district_info == nil
+          current_petition.councilperson = district_info.split("-")[1] unless district_info == nil
+       end
       end
-    end
+  #  end
   end
 end
