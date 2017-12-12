af37e711edc2f76db8f21268c4d4f84931515a0a
Heather Christman <efl7a@yahoo.de>
2017-12-12 16:25:02 +0000

    Working on Nokogiri input. Trying to deal with exceptions (.text on nil error)

--- lib/rezoning/petition.rb
+++ lib/rezoning/petition.rb
@@ -1,3 +1,12 @@
 class Rezoning::Petition
+  attr_accessor :number, :website, :petitioner, :decision, :decision_date, :description, :current_zoning, :proposed_zoning, :district, :councilperson
+  @@all = []
+    def initialize(url)
+      @website = url
+      @@all << self unless @@all.include?(self)
+    end
 
+    def self.all
+      @@all
+    end
 end
--- lib/rezoning/scraper.rb
+++ lib/rezoning/scraper.rb
@@ -3,10 +3,34 @@ require 'open-uri'
 require 'pry'
 
 class Rezoning::Scraper
+  BASE_CHARLOTTE_GOV_WEBSITE = "http://charlottenc.gov"
   def scrape_year_url
-    doc = Nokogiri::HTML(open("http://charlottenc.gov/planning/Rezoning/Pages/Home.aspx"))
-    binding.pry
-# url for year = doc.css(".dfwp-list a")[0].attribute("href").value
+    doc = Nokogiri::HTML(open("#{BASE_CHARLOTTE_GOV_WEBSITE}/planning/Rezoning/Pages/Home.aspx"))
+    # url for year = doc.css(".dfwp-list a")[0].attribute("href").value
 #              = "/planning/Rezoning/RezoningPetitions/2017Petitions/Pages/default.aspx"
+    doc.css(".dfwp-list a").map do |element|
+      element.attribute("href").value
+    end
+  end
+  def scrape_petitions
+    self.scrape_year_url.each do |year|
+      doc = Nokogiri::HTML(open("#{BASE_CHARLOTTE_GOV_WEBSITE}#{year}"))
+      rows = doc.css("tr")
+      binding.pry
+      rows[2..-1].each do |petition|
+        current_petition = Rezoning::Petition.new(petition.css("td a").attribute("href").value)
+        current_petition.number = petition.css("td")[0].text
+        current_petition.petitioner = petition.css("td")[1].text
+        current_petition.description = petition.css("td")[2].text
+        current_petition.current_zoning = petition.css("td")[2].text
+        current_petition.proposed_zoning = petition.css("td")[4].text
+        current_petition.decision = petition.css("td")[5].text
+  #      current_petition.decision_date = petition.css("td")[6].text unless petition.css("td")[6].text == nil
+        district_info = petition.css("td")[7].text
+        #"District-Councilperson"
+        current_petition.district = district_info.split("-")[0]
+        current_petition.councilperson = district_info.split("-")[1]
+      end
+    end
   end
 end
